46b2ce0a9f9c7a11ef42824f181edeee06bac6cf
JBoundless <39219944+JBoundless@users.noreply.github.com>
2021-04-07 12:14:58 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import ToDoList from "./Components/ToDoList";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="todo-app">
+      <h1>ToDo App</h1>
+      <ToDoList />
+    </div>
+  );
+}
